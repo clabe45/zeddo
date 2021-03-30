88e60fdbd848bd58238c8ae4eead9f13252f2547
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-03-30 12:32:00 -0400

    Implement opening selected article in web browser

--- hear/main.py
+++ hear/main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 import click
 import click_config_file
 
@@ -13,6 +15,10 @@ def show_top_news(top_news):
             click.style("({})".format(source), dim=True)
         ))
 
+def open_article(top_news, n):
+    article = top_news[n - 1]
+    webbrowser.open(article['url'])
+
 @click.command()
 @click.option('--api-key', help='API key for News API')
 @click.option('-n', default=5)
@@ -20,6 +26,14 @@ def show_top_news(top_news):
 def top_news(api_key, n):
     top_news = get_top_news(api_key, n)
     show_top_news(top_news)
+    n = None
+    while True:
+        n = click.prompt("Please enter an article number to open", type=int)
+        if not (0 < n <= len(top_news)):
+            click.echo("No such article: {}".format(n))
+        else:
+            break
+    open_article(top_news, n)
 
 if __name__ == '__main__':
     top_news()
