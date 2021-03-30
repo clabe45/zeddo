898d6ae5993b9ceb898ad7794b27247a1ef7fafa
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-03-30 14:33:36 -0400

    Add alias to -n option

--- zeddo/__init__.py
+++ zeddo/__init__.py
@@ -23,12 +23,12 @@ def open_article(top_news, n):
 @click.command()
 @click.option('--api-key', help='API key for News API')
 @click.option('--language', default='en')
-@click.option('-n', default=5)
+@click.option('-n', '--max-count', default=5)
 @click_config_file.configuration_option(cmd_name='zeddo')
-def top_news(api_key, language, n):
-    top_news = get_top_news(api_key, language, n)
+def top_news(api_key, language, max_count):
+    top_news = get_top_news(api_key, language, max_count)
     show_top_news(top_news)
-    n = None
+    i = None
     while True:
         s = click.prompt("Please enter an article number to open", default="")
         if s == '':
@@ -37,12 +37,12 @@ def top_news(api_key, language, n):
         if not s.isnumeric():
             click.echo("{} is not a number!".format(s))
         else:
-            n = int(s)
-            if not (0 < n <= len(top_news)):
-                click.echo("No such article: {}".format(n))
+            i = int(s)
+            if not (0 < i <= len(top_news)):
+                click.echo("No such article: {}".format(i))
             else:
                 break
-    open_article(top_news, n)
+    open_article(top_news, i)
 
 if __name__ == '__main__':
     top_news()
