a6ece4d7894e3fccb7270cf535266a969eea498e
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-03-31 11:36:32 -0400

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as r:
+    long_description = r.read()
+
 setup(
     name='zeddo',
     version='0.1.0',
@@ -10,7 +13,21 @@ setup(
         zeddo=zeddo.__init__:top_news
     ''',
     description='News CLI for lazy people',
+    long_description=long_description,
+    keywords='cli news current events tool',
     url='http://github.com/clabe45/zeddo',
     author='Caleb Sacks',
     license='GPLv3',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        "Topic :: Internet",
+        "Topic :: Office/Business :: News/Diary"
+    ]
 )
