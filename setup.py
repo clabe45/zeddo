f70da1b8acdda458bef7febbd77cfe52c65de588
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-03-31 12:49:42 -0400

    Fix classifiers in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
-        'Intended Audience :: Developers'
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
